1db648a4bddf3aa301a166c3bfe33cea2853efdf
Noah Gergel <n.gergel1@gmail.com>
2019-08-02 15:49:00 -0600

    Formatted quaternions

--- include/Quaternion.h
+++ include/Quaternion.h
@@ -5,55 +5,55 @@
 #define QUATERNION_H
 
 struct quaternion {
-	// Real/imaginary components, in the form a + bi + cj + dk.
-	float a, b, c, d;
+    // Real/imaginary components, in the form a + bi + cj + dk.
+    float a, b, c, d;
 
-	// Constructor. Defaults values to 0.
-	// @params a, b, c, d (float): The numerical components.
-	quaternion(float a = 0, float b = 0, float c = 0, float d = 0);
+    // Constructor. Defaults values to 0.
+    // @params a, b, c, d (float): The numerical components.
+    quaternion(float a = 0, float b = 0, float c = 0, float d = 0);
 
-	// Destructor. Should be empty.
-	~quaternion();
+    // Destructor. Should be empty.
+    ~quaternion();
 
-	// Assignment operator.
-	// @param u (const quaternion &): Quaternion to be assigned.
-	void operator=(const quaternion &u);
+    // Assignment operator.
+    // @param u (const quaternion &): Quaternion to be assigned.
+    void operator=(const quaternion &u);
 
-	// Addition operator.
-	// @param u (const quaternion &): quaternion to add.
-	// @return (quaternion): Sum of this + u.
-	quaternion operator+(const quaternion &u);
+    // Addition operator.
+    // @param u (const quaternion &): quaternion to add.
+    // @return (quaternion): Sum of this + u.
+    quaternion operator+(const quaternion &u);
 
-	// Subtraction operator.
-	// @param u (const quaternion &): quaternion to subtract.
-	// @return (quaternion): Difference of this - u.
-	quaternion operator-(const quaternion &u);
+    // Subtraction operator.
+    // @param u (const quaternion &): quaternion to subtract.
+    // @return (quaternion): Difference of this - u.
+    quaternion operator-(const quaternion &u);
 
-	// Scalar multiplication from the right.
-	// @param u (const float&): scalar.
-	// @return (quaternion): Scalar product of this * u.
-	quaternion operator*(const float &u);
+    // Scalar multiplication from the right.
+    // @param u (const float&): scalar.
+    // @return (quaternion): Scalar product of this * u.
+    quaternion operator*(const float &u);
 
-	// Hamilton product with quaternion u.
-	// @param u (const quaternion &): quaternion to multiply with.
-	// @return (quaternion): Hamilton product of this * u.
-	quaternion ham(const quaternion &u);
+    // Hamilton product with quaternion u.
+    // @param u (const quaternion &): quaternion to multiply with.
+    // @return (quaternion): Hamilton product of this * u.
+    quaternion ham(const quaternion &u);
 
-	// Conjugate of quaternion.
-	// @return (quaternion): The conjugate of this quaternion.
-	quaternion conj();
+    // Conjugate of quaternion.
+    // @return (quaternion): The conjugate of this quaternion.
+    quaternion conj();
 
-	// Calculate the norm of the quaternion.
-	// @return (float): The norm.
-	float norm();
+    // Calculate the norm of the quaternion.
+    // @return (float): The norm.
+    float norm();
 
-	// Calculate the versor of the quaternion.
-	// @return (quaternion): The versor of the quaternion.
-	quaternion versor();
+    // Calculate the versor of the quaternion.
+    // @return (quaternion): The versor of the quaternion.
+    quaternion versor();
 
-	// Calculate the inverse of the quaternion.
-	// @return (quaternion): The inverse of the quaternion.
-	quaternion inverse();
+    // Calculate the inverse of the quaternion.
+    // @return (quaternion): The inverse of the quaternion.
+    quaternion inverse();
 };
 
 // Scalar multiplication from the left.
--- src/Quaternion.cpp
+++ src/Quaternion.cpp
@@ -1,79 +1,75 @@
 // Quaternion implimentation.
 
 // Include statements.
-#include <cmath>
-#include <cassert>
 #include "../include/Quaternion.h"
+#include <cassert>
+#include <cmath>
 
 // Constructor.
 quaternion::quaternion(float a, float b, float c, float d) {
-	this->a = a;
-	this->b = b;
-	this->c = c;
-	this->d = d;
+    this->a = a;
+    this->b = b;
+    this->c = c;
+    this->d = d;
 }
 
 // Destructor.
-quaternion::~quaternion() {};
+quaternion::~quaternion(){};
 
 // Assignment operator.
-void quaternion::operator=(const quaternion& u) {
-	a = u.a;
-	b = u.b;
-	c = u.c;
-	d = u.d;
+void quaternion::operator=(const quaternion &u) {
+    a = u.a;
+    b = u.b;
+    c = u.c;
+    d = u.d;
 }
 
 // Addition operator.
 quaternion quaternion::operator+(const quaternion &u) {
-	return quaternion(a + u.a, b + u.b, c + u.c, d + u.d);
+    return quaternion(a + u.a, b + u.b, c + u.c, d + u.d);
 }
 
 // Subtraction operator.
 quaternion quaternion::operator-(const quaternion &u) {
-	return quaternion(a - u.a, b - u.b, c - u.c, d - u.d);
+    return quaternion(a - u.a, b - u.b, c - u.c, d - u.d);
 }
 
 // Scalar multiplication from the right.
 quaternion quaternion::operator*(const float &u) {
-	return quaternion(a * u, b * u, c * u, d * u);
+    return quaternion(a * u, b * u, c * u, d * u);
 }
 
 // Scalar multiplication from the left.
 quaternion operator*(const float &u, const quaternion &v) {
-	return quaternion(v.a * u, v.b * u, v.c * u, v.d * u);
+    return quaternion(v.a * u, v.b * u, v.c * u, v.d * u);
 }
 
 // Hamilton product.
 quaternion quaternion::ham(const quaternion &u) {
-	return quaternion((a * u.a) - (b * u.b) - (c * u.c) - (d * u.d),
-					  (a * u.b) + (b * u.a) + (c * u.d) - (d * u.c),
-					  (a * u.c) - (b * u.d) + (c * u.a) + (d * u.b),
-					  (a * u.d) + (b * u.c) - (c * u.b) + (d * u.a));
+    return quaternion((a * u.a) - (b * u.b) - (c * u.c) - (d * u.d),
+                      (a * u.b) + (b * u.a) + (c * u.d) - (d * u.c),
+                      (a * u.c) - (b * u.d) + (c * u.a) + (d * u.b),
+                      (a * u.d) + (b * u.c) - (c * u.b) + (d * u.a));
 }
 
 // Conjugate.
-quaternion quaternion::conj() {
-	return quaternion(a, -1 * b, -1 * c, -1 * d);
-}
+quaternion quaternion::conj() { return quaternion(a, -1 * b, -1 * c, -1 * d); }
 
 // Norm.
-float quaternion::norm() {
-	return (float)sqrt(a * a + b * b + c * c + d * d);
-}
+float quaternion::norm() { return (float)sqrt(a * a + b * b + c * c + d * d); }
 
 // Versor.
 quaternion quaternion::versor() {
-	// Verify the quaternion is non-zero.
-	assert(norm() != 0);
+    // Verify the quaternion is non-zero.
+    assert(norm() != 0);
 
-	return *this * (1 / norm()); 
+    return *this * (1 / norm());
 }
 
 // Inverse.
 quaternion quaternion::inverse() {
-	// Verify the quaternion is non-zero.
-	assert(norm() != 0);
+    // Verify the quaternion is non-zero.
+    assert(norm() != 0);
 
-	return conj() * (1 / (norm() * norm()));
+    return conj() * (1 / (norm() * norm()));
 }
